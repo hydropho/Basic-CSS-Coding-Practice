5d4395c0f7918494b723d28d77700412ec39eeef
hydropho <aswinkhad@gmail.com>
2022-11-16 08:31:32 +0700

    Update indentasi

--- tugas-js2/script.js
+++ tugas-js2/script.js
@@ -1,63 +1,81 @@
 let users = [
-    {
-        name: "Monica",
-        gender: "Female",
-        age: 17,
-        email: "monica@dingdong.com",
-        favoriteColor: ["Yellow", "Pink", "White", "Purple"],
-        isHavePet: "Yes",
-        education: [
-            {
-                name: "SD 01",
-                city: "Jakarta",
-                graduate: 2016
-            },
-            {
-                name: "SMP 02",
-                city: "Jakarta",
-                graduate: 2019
-            },
-            {
-                name: "SMA 03",
-                city: "Tangerang"
-            }
-        ],
-        favoriteRestaurant: [
-            "Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"
-        ]
-    },
-    {
-        name: "Wendy",
-        gender: "Male",
-        age: 23,
-        email: "wendy@dingdong.com",
-        favoriteColor: ["Blue", "Black", "Grey"],
-        isHavePet: "No",
-        education: [
-            {
-                name: "SD 02",
-                city: "Jakarta",
-                graduate: 2010
-            },
-            {
-                name: "SMP 03",
-                city: "Bogor",
-                graduate: 2013
-            },
-            {
-                name: "SMA 01",
-                city: "Surabaya",
-                graduate: 2016
-            },
-            {
-                name: "Universitas Maju",
-                city: "Tangerang"
-            }
-        ],
-        favoriteRestaurant: [
-            "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
-        ]
-    }
+  {
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: ["Yellow", "Pink", "White", "Purple"],
+    isHavePet: "Yes",
+    education: [
+      {
+        name: "SD 01",
+        city: "Jakarta",
+        graduate: 2016,
+      },
+      {
+        name: "SMP 02",
+        city: "Jakarta",
+        graduate: 2019,
+      },
+      {
+        name: "SMA 03",
+        city: "Tangerang",
+      },
+    ],
+    favoriteRestaurant: [
+      "Bento",
+      "Sushi",
+      "Pancake",
+      "Eggy",
+      "Tempura",
+      "Bento",
+      "Eggy",
+      "Padang",
+      "Tteok",
+      "Sushi",
+      "Sushi",
+    ],
+  },
+  {
+    name: "Wendy",
+    gender: "Male",
+    age: 23,
+    email: "wendy@dingdong.com",
+    favoriteColor: ["Blue", "Black", "Grey"],
+    isHavePet: "No",
+    education: [
+      {
+        name: "SD 02",
+        city: "Jakarta",
+        graduate: 2010,
+      },
+      {
+        name: "SMP 03",
+        city: "Bogor",
+        graduate: 2013,
+      },
+      {
+        name: "SMA 01",
+        city: "Surabaya",
+        graduate: 2016,
+      },
+      {
+        name: "Universitas Maju",
+        city: "Tangerang",
+      },
+    ],
+    favoriteRestaurant: [
+      "Tempura",
+      "Bento",
+      "Sushi",
+      "Pancake",
+      "Padang",
+      "Katsu",
+      "Geprek",
+      "Pancake",
+      "Eggy",
+    ],
+  },
 ];
 
 console.log("Nama : " + users[1].name);
